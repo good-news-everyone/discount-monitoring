d0a493aa05b4c7fc8c039b57bd6382570e5fb99a
Alikutov <likutov.alexey@gmail.com>
2020-12-05 16:21:05 +0300

    add on delete cascade

--- src/main/resources/db/migration/V1_2__Add_price_currency.sql
+++ src/main/resources/db/migration/V1_2__Add_price_currency.sql
@@ -1,2 +1,9 @@
 alter table items
-    add column price_currency varchar default 'RUB';
+    add column if not exists price_currency varchar default 'RUB';
+
+ALTER TABLE price_changelog
+    DROP CONSTRAINT price_changelog_item_id_fkey;
+ALTER TABLE price_changelog
+    ADD CONSTRAINT price_changelog_item_id_fkey FOREIGN KEY (item_id)
+        REFERENCES items (id) MATCH SIMPLE
+        ON UPDATE CASCADE ON DELETE CASCADE;
